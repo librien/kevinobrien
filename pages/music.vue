a0df6887fa8b2b52c5cba3f2396eb3e689f7af6d
Kevin O'Brien <kevin.matthew.obrien@gmail.com>
2021-06-12 01:19:31 -0400

    Add wavesurfer component

--- pages/music.vue
+++ pages/music.vue
@@ -1,19 +1,51 @@
 <template>
-  <v-row>
-    <v-col class="text-center">
-      <img
-        src="/v.png"
-        alt="Vuetify.js"
-        class="mb-5"
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <wavesurfer :src="file" :options="options" style="background-color: rgba(0,0,0,.05)"></wavesurfer>
+      <v-card
+        class="mx-auto"
       >
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
+        <v-img
+          src="https://cdn.vuetifyjs.com/images/lists/ali.png"
+          height="300px"
+          dark
+        >
+          <v-row class="fill-height">
+            <v-card-title>
+            </v-card-title>
+
+          </v-row>
+        </v-img>
+
+        <v-list two-line>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon color="black">
+                mdi-play
+              </v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>Song Title</v-list-item-title>
+              <v-list-item-subtitle>Length</v-list-item-subtitle>
+            </v-list-item-content>
+
+            <v-list-item-icon>
+              <v-icon>mdi-message-text</v-icon>
+            </v-list-item-icon>
+          </v-list-item>
+        </v-list>
+      </v-card>
     </v-col>
   </v-row>
 </template>
+<script>
+  export default {
+    data() {
+      return {
+        options: {},
+        file: "http://localhost:3000/media/facewave.mp3",
+      };
+    },
+  }
+</script>
