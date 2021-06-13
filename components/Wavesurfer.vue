c4d1c2998e3145e5b410f4e023289989ad1e930a
Kevin O'Brien <kevin.matthew.obrien@gmail.com>
2021-06-12 23:08:48 -0400

    Extract Wavesurfer component

---
+++ components/Wavesurfer.vue
@@ -0,0 +1,22 @@
+<template>
+    <wavesurfer :src="file" :options="options" style="background-color: rgba(0,0,0,.05)" ref="surf"></wavesurfer>
+</template>
+<script>
+export default {
+  mounted() {
+    this.player.on('ready', () => {
+    })
+  },
+  computed: {
+    player() {
+      return this.$refs.surf.waveSurfer
+    }
+  },
+  data() {
+    return {
+        options: {},
+        file: "http://localhost:3000/media/facewave.mp3",
+        };
+    },
+}
+</script>
