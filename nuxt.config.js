cee40d3ebff140b63f61e1de1d950aefff554404
Kevin O'Brien <kevin.matthew.obrien@gmail.com>
2021-06-14 01:35:48 -0400

    Replace wavesurfer-vue-js with wavesurfer.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,9 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ["~/plugins/wavesurfer.js-vue"],
+  plugins: [
+    //"~/plugins/wavesurfer.js-vue"
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
