00554a3fe360dcab0f9c3a30f0c79480c6c02313
Kevin O'Brien <kevin.matthew.obrien@gmail.com>
2021-06-15 21:46:26 -0400

    Add transparency and container to footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,8 +1,22 @@
 <template>
-  <v-footer
-    :absolute="!fixed"
-    app
-  >
-    <span>&copy; {{ new Date().getFullYear() }} Kevin O'Brien</span>
+  <v-footer absolute app>
+    <v-container>
+      <v-row justify="center" align="center">
+        <v-col
+          cols="12"
+          sm="8"
+          md="6"
+          style="padding-top: 0px; padding-bottom: 0px"
+        >
+          <span style="font-size: .7rem;">&copy; {{ new Date().getFullYear() }} Kevin O'Brien</span>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-footer>
 </template>
+<style lang="scss" scoped>
+.v-footer.theme--light.v-sheet {
+  background-color: rgba(255, 255, 255, 0.75) !important;
+  color: black;
+}
+</style>
