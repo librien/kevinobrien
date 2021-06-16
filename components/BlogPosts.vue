2b53949df660a927ded95bc23ff9c7fd719aac0a
Kevin O'Brien <kevin.matthew.obrien@gmail.com>
2021-06-15 23:42:04 -0400

    Create component for blog posts

---
+++ components/BlogPosts.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <v-card v-for="post in posts" :key="post.id">
+      <div>{{ post.title.rendered }}</div>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    posts: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,23 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-      <v-card
-          class="mx-auto"
-        >
-          <v-card-title>Card title</v-card-title>
-
-          <v-card-text>
-            Phasellus magna. Quisque rutrum. Nunc egestas, augue at pellentesque laoreet, felis eros vehicula leo, at malesuada velit leo quis pede. Aliquam lobortis. Quisque libero metus, condimentum nec, tempor a, commodo mollis, magna.
-
-            In turpis. In dui magna, posuere eget, vestibulum et, tempor auctor, justo. In turpis. Pellentesque dapibus hendrerit tortor. Ut varius tincidunt libero.
-          </v-card-text>
-        </v-card>
+      <blog-posts :posts="posts"/>
     </v-col>
   </v-row>
 </template>
 
 <script>
-
+import BlogPosts from '~/components/BlogPosts.vue';
+export default {
+  components: { BlogPosts },
+  methods: {
+    async getPosts() {
+      const posts = await this.$axios.$get('https://gorillalogic.com/wp-json/wp/v2/posts')
+      this.posts = posts
+    }
+  },
+  mounted() {
+    this.getPosts();
+  }
+}
 </script>
