1a76f323afd24f9dbfae2b8749cae3edca2cf3f8
Kevin O'Brien <kevin.matthew.obrien@gmail.com>
2021-06-20 00:17:46 -0400

    Add spacing for blog post cards

--- components/BlogPosts.vue
+++ components/BlogPosts.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
-    <v-card v-for="post in posts" :key="post.id">
-      <div>{{ post }}</div>
+    <v-card v-for="post in posts" :key="post.id" style="margin-bottom: 20px">
+      <v-card-title to="/">{{post.title}}</v-card-title>
+      <v-card-subtitle>{{post.body}}</v-card-subtitle>
+      <v-card-actions>
+        <v-btn
+          outlined
+          rounded
+          text
+        >
+          Button
+        </v-btn>
+      </v-card-actions>
     </v-card>
   </div>
 </template>
